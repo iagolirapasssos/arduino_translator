c6e3fb72901ab21d233fe1fe7cc934a604635aaa
iagolirapassos <iagolirapassos@gmail.com>
2024-06-21 19:04:58 -0300

    Correções gerais

--- arduino_translator/translator.py
+++ arduino_translator/translator.py
@@ -7,7 +7,8 @@ class ArduinoTranslator:
     
     def translate(self, custom_code):
         arduino_code = custom_code
-        for custom_command, arduino_command in self.commands.items():
+        for custom_command, details in self.commands.items():
+            arduino_command = details['translation']
             # Usando regex para substituir apenas palavras completas
             arduino_code = re.sub(r'\b' + re.escape(custom_command) + r'\b', arduino_command, arduino_code)
         return arduino_code
