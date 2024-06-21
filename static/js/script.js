db07fbc029115b12f5d7dc7ea28fd81b7a195042
iagolirapassos <iagolirapassos@gmail.com>
2024-06-21 20:56:42 -0300

    Sistema completo

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,47 @@
 function initializeEditor(keywords) {
     let customKeywords = {};
-    keywords.forEach(keyword => {
-        customKeywords[keyword] = "keyword";
-    });
+    for (const keyword in keywords) {
+        let category = keywords[keyword];
+        customKeywords[keyword] = `keyword-${category}`;
+    }
 
     CodeMirror.defineMIME("text/x-custom", {
         name: "clike",
         keywords: customKeywords
     });
 
-    let editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
+    let editor = CodeMirror.fromTextArea(document.getElementById('editor-textarea'), {
         lineNumbers: true,
         mode: "text/x-custom",
         theme: "darcula"
     });
 
-    return editor;
+    let translation = CodeMirror.fromTextArea(document.getElementById('translation-textarea'), {
+        lineNumbers: true,
+        mode: "text/x-c++src",
+        theme: "darcula",
+        readOnly: true
+    });
+
+    editor.on('change', function() {
+        updateTranslation(editor, translation);
+    });
+
+    return { editor, translation };
 }
 
 function fetchKeywordsAndInitializeEditor() {
     fetch('http://162.248.101.160:5000/keywords')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(keywords => {
-            let editor = initializeEditor(Object.keys(keywords));
+            let { editor, translation } = initializeEditor(keywords);
 
-            document.querySelector('.menu-item[onclick="newFile()"]').onclick = () => newFile(editor);
+            document.querySelector('.menu-item[onclick="newFile()"]').onclick = () => newFile(editor, translation);
             document.querySelector('.menu-item[onclick="openFile()"]').onclick = () => openFile(editor);
             document.querySelector('.menu-item[onclick="saveFile()"]').onclick = () => saveFile(editor);
             document.querySelector('.menu-item[onclick="saveFileAs()"]').onclick = () => saveFileAs(editor);
@@ -35,8 +52,25 @@ function fetchKeywordsAndInitializeEditor() {
         });
 }
 
-function newFile(editor) {
+function updateTranslation(editor, translation) {
+    let content = editor.getValue();
+    fetch('http://162.248.101.160:5000/translate', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ code: content })
+    })
+    .then(response => response.text())
+    .then(translatedCode => {
+        translation.setValue(translatedCode);
+    })
+    .catch(error => {
+        console.error('Erro na tradução:', error);
+    });
+}
+
+function newFile(editor, translation) {
     editor.setValue('');
+    translation.setValue('');
 }
 
 function openFile(editor) {
