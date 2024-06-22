dcc226dee5ea6b23ca98c2fd99f572f50d740b70
iagolirapassos <iagolirapassos@gmail.com>
2024-06-21 22:35:21 -0300

    Update server and script to use HTTPS

--- static/js/script.js
+++ static/js/script.js
@@ -31,7 +31,7 @@ function initializeEditor(keywords) {
 }
 
 function fetchKeywordsAndInitializeEditor() {
-    fetch('162.248.101.160:5000/keywords')
+    fetch('http://162.248.101.160:5000/keywords')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -55,7 +55,7 @@ function fetchKeywordsAndInitializeEditor() {
 
 function updateTranslation(editor, translation) {
     let content = editor.getValue();
-    fetch('162.248.101.160:5000/translate', {
+    fetch('http://162.248.101.160:5000/translate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ code: content })
@@ -113,7 +113,7 @@ function saveFileAs(editor) {
 
 function translateCode(editor) {
     let content = editor.getValue();
-    fetch('162.248.101.160:5000/translate', {
+    fetch('http://162.248.101.160:5000/translate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ code: content })
