934820c6e2fb7f606aac9d72168fb92eaafc0ffd
iagolirapassos <iagolirapassos@gmail.com>
2024-06-22 11:16:57 -0300

    Server update

--- static/js/script.js
+++ static/js/script.js
@@ -31,7 +31,7 @@ function initializeEditor(keywords) {
 }
 
 function fetchKeywordsAndInitializeEditor() {
-    fetch('http://162.248.101.160:5000/keywords')
+    fetch('https://bosonshiggs.com.br/keywords')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -52,10 +52,9 @@ function fetchKeywordsAndInitializeEditor() {
         });
 }
 
-
 function updateTranslation(editor, translation) {
     let content = editor.getValue();
-    fetch('http://162.248.101.160:5000/translate', {
+    fetch('https://bosonshiggs.com.br/translate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ code: content })
@@ -113,7 +112,7 @@ function saveFileAs(editor) {
 
 function translateCode(editor) {
     let content = editor.getValue();
-    fetch('http://162.248.101.160:5000/translate', {
+    fetch('https://bosonshiggs.com.br/translate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ code: content })
